9fbf65d21f634404de17671a85353130612b3f0c
Emilse Arias <emilsearias@gmail.com>
2022-08-29 10:39:41 -0300

    estamos preparando la funcion actualizarPelicula

--- js/admin.js
+++ js/admin.js
@@ -1,38 +1,45 @@
 import { Pelicula } from "./classPelicula.js";
 
 //aqui voy a guardar todas las peliculas
-let listaPeliculas = JSON.parse(localStorage.getItem('listaPeliculasKey')) || []; 
+let listaPeliculas =
+  JSON.parse(localStorage.getItem("listaPeliculasKey")) || [];
 
 //traer los input
-let codigo = document.querySelector('#codigo');
-let titulo = document.querySelector('#titulo');
-let descripcion = document.querySelector('#descripcion');
-let imagen = document.querySelector('#imagen');
-let genero = document.querySelector('#genero');
-let formPelicula = document.querySelector('#formPelicula');
-let btnCrearPelicula = document.querySelector('#btnCrearPelicula');
+let codigo = document.querySelector("#codigo");
+let titulo = document.querySelector("#titulo");
+let descripcion = document.querySelector("#descripcion");
+let imagen = document.querySelector("#imagen");
+let genero = document.querySelector("#genero");
+let formPelicula = document.querySelector("#formPelicula");
+let btnCrearPelicula = document.querySelector("#btnCrearPelicula");
 // crear una instancia de la ventana modal
-const modalAdminPelicula = new bootstrap.Modal(document.querySelector('#modalPelicula'));
+const modalAdminPelicula = new bootstrap.Modal(
+  document.querySelector("#modalPelicula")
+);
+//variable para ejecutar la funcion que crea peliculas o la que actualiza
+let peliculaNueva = true; // si peliculaNueva es true creo una pelicula, caso contrario actualizo
 
 //agregar el evento
 
-btnCrearPelicula.addEventListener('click', crearPelicula);
-formPelicula.addEventListener('submit', guardarPelicula);
+btnCrearPelicula.addEventListener("click", crearPelicula);
+formPelicula.addEventListener("submit", guardarPelicula);
 
 cargarInicial();
 
-function cargarInicial(){
-    if(listaPeliculas.length > 0){
-        //dibujar las filas de la tabla
-        listaPeliculas.forEach((itemPelicula)=>{ crearFila(itemPelicula) })
-    }
+function cargarInicial() {
+  if (listaPeliculas.length > 0) {
+    //dibujar las filas de la tabla
+    listaPeliculas.forEach((itemPelicula) => {
+      crearFila(itemPelicula);
+    });
+  }
 }
 
-function crearFila(pelicula){
-    //esta funcion dibuja un tr
-    let tablaPeliculas = document.querySelector('#tablaPeliculas');
-    //creamos el tr con document.createElement o innerHTML del tbody
-    tablaPeliculas.innerHTML += `<tr>
+function crearFila(pelicula) {
+  //esta funcion dibuja un tr
+  let tablaPeliculas = document.querySelector("#tablaPeliculas");
+  //creamos el tr con document.createElement o innerHTML del tbody
+  tablaPeliculas.innerHTML += `<tr>
     <th scope="row">${pelicula.codigo}</th>
     <td>${pelicula.titulo}</td>
     <td>${pelicula.descripcion}</td>
@@ -46,98 +53,134 @@ function crearFila(pelicula){
         <i class="bi bi-x-square"></i>
       </button>
     </td>
-  </tr>`
-    console.log(tablaPeliculas)
+  </tr>`;
+  console.log(tablaPeliculas);
 }
 
-function crearPelicula(){
-    //mostrar ventana modal
-    modalAdminPelicula.show();
-    //generar el identificador unico y asignarlo al campo del codigo
-    codigo.value = uuidv4();
-    // console.log( uuidv4()); esta libreria genera identificadores unicos
+function crearPelicula() {
+  //volver asignar a la variable booleana el valor true
+  peliculaNueva = true;
+  //mostrar ventana modal
+  modalAdminPelicula.show();
+  //generar el identificador unico y asignarlo al campo del codigo
+  codigo.value = uuidv4();
+  // console.log( uuidv4()); esta libreria genera identificadores unicos
 }
 
-function guardarPelicula(e){
-    e.preventDefault();
-   //volver a validar todos los campos
+function guardarPelicula(e) {
+  e.preventDefault();
+  //volver a validar todos los campos
 
-   //si los datos son correctos
-   let nuevaPelicula =  new Pelicula(codigo.value, titulo.value, descripcion.value, imagen.value, genero.value);
-   console.log(nuevaPelicula)
-   listaPeliculas.push(nuevaPelicula);
-   //guardar el arreglo en localstorage
-   guardarPeliculasEnLocalStorage();
-   //limpiar formulario
-   limpiarFormulario();
-   console.log(listaPeliculas);
-   //dibujar la fila en la tabla
-   crearFila(nuevaPelicula)
-   //cerrar la ventana modal
-   modalAdminPelicula.hide();
+  //    if(peliculaNueva === true){
+  if (peliculaNueva) {
+    generarPeliculaNueva();
+  } else {
+    actualizarPelicula();
+  }
 }
 
-function limpiarFormulario(){
-    formPelicula.reset()
-    // modificar las clases de bootstrap si es necesario
+function generarPeliculaNueva() {
+  //si los datos son correctos
+  let nuevaPelicula = new Pelicula(
+    codigo.value,
+    titulo.value,
+    descripcion.value,
+    imagen.value,
+    genero.value
+  );
+  console.log(nuevaPelicula);
+  listaPeliculas.push(nuevaPelicula);
+  //guardar el arreglo en localstorage
+  guardarPeliculasEnLocalStorage();
+  //limpiar formulario
+  limpiarFormulario();
+  console.log(listaPeliculas);
+  //dibujar la fila en la tabla
+  crearFila(nuevaPelicula);
+  //cerrar la ventana modal
+  modalAdminPelicula.hide();
 }
 
-
-function guardarPeliculasEnLocalStorage(){
-    localStorage.setItem('listaPeliculasKey', JSON.stringify(listaPeliculas));
+function limpiarFormulario() {
+  formPelicula.reset();
+  // modificar las clases de bootstrap si es necesario
 }
 
-window.borrarPelicula = function (codigo){
-    //mostrar una pregunta al usuario
-    Swal.fire({
-        title: 'Eliminar pelicula',
-        text: "Esta seguro de eliminar la pelicula, este proceso no se puede revertir",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Borrar',
-        cancelButtonText: 'Cancelar'
-      }).then((result) => {
-        console.log(result)
-        if (result.isConfirmed) {
-            //aqui quiero borrar la pelicula
-            // console.log(codigo)
-            //buscar la pelicula en el arreglo y borrarla
-            let copiaListaPeliculas = listaPeliculas.filter((itemPelicula)=> itemPelicula.codigo != codigo);
-            listaPeliculas = copiaListaPeliculas;
-            //actualizar el localstorage
-            guardarPeliculasEnLocalStorage();
-            //actualizar la tabla
-            borrarTabla();
-            cargarInicial();
-
-            // mostrar mensaje de operacion correcta
-          Swal.fire(
-            'Pelicula eliminada',
-            'La pelicula seleccionada fue correctamente eliminada',
-            'success'
-          )
-        }
-      })
+function guardarPeliculasEnLocalStorage() {
+  localStorage.setItem("listaPeliculasKey", JSON.stringify(listaPeliculas));
 }
 
-function borrarTabla(){
-    let tablaPeliculas = document.querySelector('#tablaPeliculas');
-    tablaPeliculas.innerHTML = '';
+window.borrarPelicula = function (codigo) {
+  //mostrar una pregunta al usuario
+  Swal.fire({
+    title: "Eliminar pelicula",
+    text: "Esta seguro de eliminar la pelicula, este proceso no se puede revertir",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Borrar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    console.log(result);
+    if (result.isConfirmed) {
+      //aqui quiero borrar la pelicula
+      // console.log(codigo)
+      //buscar la pelicula en el arreglo y borrarla
+      let copiaListaPeliculas = listaPeliculas.filter(
+        (itemPelicula) => itemPelicula.codigo != codigo
+      );
+      listaPeliculas = copiaListaPeliculas;
+      //actualizar el localstorage
+      guardarPeliculasEnLocalStorage();
+      //actualizar la tabla
+      borrarTabla();
+      cargarInicial();
+
+      // mostrar mensaje de operacion correcta
+      Swal.fire(
+        "Pelicula eliminada",
+        "La pelicula seleccionada fue correctamente eliminada",
+        "success"
+      );
+    }
+  });
+};
+
+function borrarTabla() {
+  let tablaPeliculas = document.querySelector("#tablaPeliculas");
+  tablaPeliculas.innerHTML = "";
 }
 
-window.editarPelicula = function (codigoBuscado){
-    console.log(codigoBuscado)
-    //buscar dentro del arreglo la pelicula que quiero editar
-// let peliculaBuscada = listaPeliculas.find((pelicula)=> {return pelicula.codigo === codigo})
-    let peliculaBuscada = listaPeliculas.find((pelicula)=> pelicula.codigo === codigoBuscado); //return implicito
-    console.log(peliculaBuscada);
-    //mostrar la ventana modal con el formulario cargado con todos los datos de la pelicula que selecciono el usuario
-    modalAdminPelicula.show();
-    codigo.value = peliculaBuscada.codigo;
-    titulo.value = peliculaBuscada.titulo;
-    descripcion.value = peliculaBuscada.descripcion;
-    imagen.value = peliculaBuscada.imagen;
-    genero.value = peliculaBuscada.genero;
+window.editarPelicula = function (codigoBuscado) {
+  // console.log(codigoBuscado)
+  //cambiar el estado de la variable booleada peliculaNueva
+  peliculaNueva = false;
+  //buscar dentro del arreglo la pelicula que quiero editar
+  // let peliculaBuscada = listaPeliculas.find((pelicula)=> {return pelicula.codigo === codigo})
+  let peliculaBuscada = listaPeliculas.find(
+    (pelicula) => pelicula.codigo === codigoBuscado
+  ); //return implicito
+  console.log(peliculaBuscada);
+  //mostrar la ventana modal con el formulario cargado con todos los datos de la pelicula que selecciono el usuario
+  modalAdminPelicula.show();
+  codigo.value = peliculaBuscada.codigo;
+  titulo.value = peliculaBuscada.titulo;
+  descripcion.value = peliculaBuscada.descripcion;
+  imagen.value = peliculaBuscada.imagen;
+  genero.value = peliculaBuscada.genero;
+};
+
+function actualizarPelicula() {
+  console.log("actualizando pelicula...");
+  //tomar todos los datos cargados del formulario, buscar el objeto que estoy mostrando en el formulario y actualizar sus valores
+
+  //actualizar el localstorage
+
+  //actualizar la tabla
+
+  //cerrar la ventana modal
+
+  //limpiar el formulario
+  limpiarFormulario();
 }
